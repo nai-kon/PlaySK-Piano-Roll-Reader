9cd489159f1427df8c75af0fa34e666fabd9e609
nai-kon <fxtch686@yahoo.co.jp>
2024-06-01 16:11:03 +0900

    add Themodist tracker bar

--- src/players/Themodist.py
+++ src/players/Themodist.py
@@ -8,16 +8,21 @@ class Themodist(BasePlayer):
         super().__init__(confpath, midiobj)
         with open(confpath, encoding="utf-8") as f:
             conf = json.load(f)
-        self.accomp_vacuum = conf["expression"]["base"]
-        self.theme_vacuum = conf["expression"]["accent"]
+        self.base_vacuum = conf["expression"]["base"]
+        self.accent_vacuum = conf["expression"]["accent"]
+        self.bass_vacuum = self.treble_vacuum = self.base_vacuum
+
+    def emulate_off(self):
+        super().emulate_off()
+        self.bass_vacuum = self.treble_vacuum = self.base_vacuum
 
     def emulate_expression(self, curtime):
-        self.bass_vacuum = self.treble_vacuum = self.accomp_vacuum
+        self.bass_vacuum = self.treble_vacuum = self.base_vacuum
         if self.holes["bass_snakebite"]["is_open"]:
-            self.bass_vacuum = self.theme_vacuum
+            self.bass_vacuum = self.accent_vacuum
 
         if self.holes["treble_snakebite"]["is_open"]:
-            self.treble_vacuum = self.theme_vacuum
+            self.treble_vacuum = self.accent_vacuum
 
 
 if __name__ == "__main__":
