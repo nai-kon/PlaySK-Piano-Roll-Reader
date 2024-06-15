a36ceec27388c68bc589d54cec568c75c37a7129
nai-kon <fxtch686@yahoo.co.jp>
2024-06-15 22:36:43 +0900

    update unittest

--- test/test_controls.py
+++ test/test_controls.py
@@ -15,7 +15,7 @@ class TestNotifyUpdate:
 
         # return "X.Y"
         ver = obj.fetch_latest_version()
-        assert re.match(r"\d\.\d", ver) is not None
+        assert re.match(r"\d\.\d\.\d", ver) is not None
 
         # invalid release title return None
         monkeypatch.setattr(json, "loads", lambda self: {"name": "Draft"})
@@ -34,37 +34,37 @@ class TestNotifyUpdate:
         conf = ConfigMng()
         obj = NotifyUpdate(None, conf)
 
-        conf.update_notified_version = "3.0"
-        mocker.patch("controls.APP_VERSION", new="3.0")
+        conf.update_notified_version = "3.3.0"
+        mocker.patch("controls.APP_VERSION", new="3.3.0")
         # not notify
-        assert not obj.need_notify("3.0")
+        assert not obj.need_notify("3.3.0")
         assert not obj.need_notify(None)
         # notify
-        assert obj.need_notify("3.1")
+        assert obj.need_notify("3.4.0")
         # already notified this version. skip
-        conf.update_notified_version = "3.1"
-        assert not obj.need_notify("3.1")
+        conf.update_notified_version = "3.4.0"
+        assert not obj.need_notify("3.4.0")
         # more new version is coming. notify
-        assert obj.need_notify("3.2")
+        assert obj.need_notify("3.4.1")
 
     def test_notify(self, mocker):
         conf = ConfigMng()
-        conf.update_notified_version = "3.0"
+        conf.update_notified_version = "3.3.0"
         wxcallafter_mock = mocker.patch("wx.CallAfter")
         obj = NotifyUpdate(None, conf)
-        obj.notify("3.1")
+        obj.notify("3.4.0")
         # version in conf is updated
-        assert conf.update_notified_version == "3.1"
+        assert conf.update_notified_version == "3.4.0"
         # call notify dlg with desired param
-        wxcallafter_mock.assert_called_once_with(NotifyDialog, None, "3.1")
+        wxcallafter_mock.assert_called_once_with(NotifyDialog, None, "3.4.0")
 
     def test_check(self, mocker, monkeypatch):
         # integration testing
         conf = ConfigMng()
-        conf.update_notified_version = "3.0"
-        mocker.patch("controls.APP_VERSION", new="3.0")
+        conf.update_notified_version = "3.3.0"
+        mocker.patch("controls.APP_VERSION", new="3.3.0")
         wxcallafter_mock = mocker.patch("wx.CallAfter")
-        monkeypatch.setattr(NotifyUpdate, "fetch_latest_version", lambda self: "3.0")
+        monkeypatch.setattr(NotifyUpdate, "fetch_latest_version", lambda self: "3.3.0")
 
         # not notify
         start = time.time()
@@ -75,10 +75,10 @@ class TestNotifyUpdate:
         wxcallafter_mock.assert_not_called()
 
         # notify
-        monkeypatch.setattr(NotifyUpdate, "fetch_latest_version", lambda self: "3.1")
+        monkeypatch.setattr(NotifyUpdate, "fetch_latest_version", lambda self: "3.4.0")
         start = time.time()
         th = NotifyUpdate.check(None, conf)
         end = time.time()
         th.join()
         assert end - start < 0.5  # return immediately
-        wxcallafter_mock.assert_called_once_with(NotifyDialog, None, "3.1")
+        wxcallafter_mock.assert_called_once_with(NotifyDialog, None, "3.4.0")
