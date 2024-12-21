dff2652aeb6b871111dd5e176149f714c997eca3
nai-kon <fxtch686@yahoo.co.jp>
2024-12-21 17:16:58 +0900

    add unittest of Welte Licensee and Themodist e-valve

---
+++ test/test_Themodist_eValve.py
@@ -0,0 +1,79 @@
+import sys
+
+import numpy as np
+import pytest
+
+sys.path.append("src/")
+from midi_controller import MidiWrap
+from tracker_bars.Themodist_eValve import Themodist_eValve
+
+
+class TestThemodist_eValve:
+    @pytest.fixture
+    def player(self) -> Themodist_eValve:
+        midiobj = MidiWrap()
+        return Themodist_eValve("src/playsk_config/Themodist e-Valve.json", midiobj)
+
+    def test_emulate_off(self, player):
+        player.bass_vacuum = player.treble_vacuum = player.accent_vacuum
+        player.emulate_off()
+        assert player.bass_vacuum == player.base_vacuum
+        assert player.treble_vacuum == player.base_vacuum
+
+    def test_accent(self, player, mocker):
+        # bass accent on
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["bass_snakebite"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_on")
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        midi_mock.assert_called_with(player.bass_snake_midi_no, velocity=64)
+        assert player.bass_vacuum == player.accent_vacuum
+        assert player.treble_vacuum == player.base_vacuum
+
+        # bass accent off
+        frame[y1:y2, x1:x2, :] = 0
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_off")
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        midi_mock.assert_called_with(player.bass_snake_midi_no, velocity=64)
+        assert player.bass_vacuum == player.base_vacuum
+        assert player.treble_vacuum == player.base_vacuum
+
+        # treble accent on
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["treble_snakebite"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_on")
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        midi_mock.assert_called_with(player.treble_snake_midi_no, velocity=64)
+        assert player.bass_vacuum == player.base_vacuum
+        assert player.treble_vacuum == player.accent_vacuum
+
+        # treble accent off
+        frame[y1:y2, x1:x2, :] = 0
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_off")
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        midi_mock.assert_called_with(player.treble_snake_midi_no, velocity=64)
+        assert player.bass_vacuum == player.base_vacuum
+        assert player.treble_vacuum == player.base_vacuum
+
+    def test_sustain(self, player, mocker):
+        # sustein on
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["sustain"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_on")
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        midi_mock.assert_called_with(player.sustein_midi_no, velocity=64)
+
+        # sustein off
+        frame[y1:y2, x1:x2, :] = 0
+        midi_mock = mocker.patch("midi_controller.MidiWrap.note_off")
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        midi_mock.assert_called_with(player.sustein_midi_no, velocity=64)
---
+++ test/test_WelteLicensee.py
@@ -0,0 +1,133 @@
+import sys
+
+import numpy as np
+import pytest
+
+sys.path.append("src/")
+import tracker_bars
+from midi_controller import MidiWrap
+
+
+class TestWelteLicensee:
+    @pytest.fixture
+    def player(self):
+        midiobj = MidiWrap()
+        obj = tracker_bars.WelteLicensee("src/playsk_config/Welte Licensee white back.json", midiobj)
+        return obj
+
+    def test_emulate_off(self, player):
+        player.bass_cres_pos = 1
+        player.bass_cres_state = "slow_cres"
+        player.bass_mf_hook = True
+        player.treble_cres_pos = 1
+        player.treble_cres_state = "slow_cres"
+        player.treble_mf_hook = True
+        player.bass_vacuum = 30
+        player.treble_vacuum = 30
+        player.emulate_off()
+        assert player.bass_cres_pos == 0
+        assert player.bass_cres_state == "slow_decres"
+        assert not player.bass_mf_hook
+        assert player.treble_cres_pos == 0
+        assert player.treble_cres_state == "slow_decres"
+        assert not player.treble_mf_hook
+        assert player.bass_vacuum == player.min_vacuum
+        assert player.treble_vacuum == player.min_vacuum
+
+    def test_pedal(self, player, mocker):
+        # sustain on
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        sustain_on_mock = mocker.patch("midi_controller.MidiWrap.sustain_on")
+        x1, y1, x2, y2 = player.holes["sustain_on"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        sustain_on_mock.assert_called_once()
+
+        # sustain off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        sustain_off_mock = mocker.patch("midi_controller.MidiWrap.sustain_off")
+        x1, y1, x2, y2 = player.holes["sustain_off"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        sustain_off_mock.assert_called_once()
+
+        # hammer rail on
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        sustain_on_mock = mocker.patch("midi_controller.MidiWrap.hammer_lift_on")
+        x1, y1, x2, y2 = player.holes["soft_on"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        sustain_on_mock.assert_called_once()
+
+        # hammer rail off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        sustain_off_mock = mocker.patch("midi_controller.MidiWrap.hammer_lift_off")
+        x1, y1, x2, y2 = player.holes["soft_off"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_pedals()
+        sustain_off_mock.assert_called_once()
+
+    def test_emulate_expression(self, player):
+        # bass mf on/off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["bass_mf_on"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.bass_mf_hook
+
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["bass_mf_off"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert not player.bass_mf_hook
+
+        # bass slow crescend on/off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["bass_cresc_forte"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.bass_cres_state == "slow_cres"
+
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["bass_cresc_piano"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.bass_cres_state == "slow_decres"
+
+        # treble mf on/off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["treble_mf_on"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.treble_mf_hook
+
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["treble_mf_off"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert not player.treble_mf_hook
+
+        # treble slow crescend on/off
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["treble_cresc_forte"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.treble_cres_state == "slow_cres"
+
+        frame = np.full((600, 800, 3), 0, np.uint8)
+        x1, y1, x2, y2 = player.holes["treble_cresc_piano"]["pos"][0]
+        frame[y1:y2, x1:x2, :] = 255
+        player.holes.set_frame(frame, 0)
+        player.emulate_expression(0)
+        assert player.treble_cres_state == "slow_decres"
